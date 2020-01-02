5c3308ce469c07dfbd95d4f812ab63ce3c83897e
Mariusz Gil <mariusz.gil91@gmail.com>
2020-01-02 22:50:03 +0100

    create HomePage.java and first TestCase

--- src/FirstTests/OpenMainPage.java
+++ src/FirstTests/OpenMainPage.java
@@ -1,34 +1,38 @@
 package FirstTests;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
 import java.util.concurrent.TimeUnit;
 
 public class OpenMainPage {
 
     WebDriver driver;
+    WebDriverWait myWait;
     String loginPage = "http://automationpractice.com";
     String expectedTitle = "My Store";
 
 
     @BeforeTest
     public void setBaseURL(){
-        System.setProperty("webdriver.chrome.driver","/Users/mariuszgil/Developer/automationPractice/chromedriver-2");
+        System.setProperty("webdriver.chrome.driver","/Users/mariuszgil/Developer/automationPractice/chromedriver-4");
         driver = new ChromeDriver();
+        myWait = new WebDriverWait(driver, 10);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
-
     }
 
     @Test
     public void verifyTitleHomePage(){
         driver.get(loginPage);
         String actualTitle = driver.getTitle();
+        //myWait.until(ExpectedConditions.visibilityOf(By.id("username")));
         Assert.assertEquals(actualTitle, expectedTitle);
     }
 
---
+++ src/Pages/HomePage.java
@@ -0,0 +1,35 @@
+package Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+
+public class HomePage {
+
+    private final WebDriver driver;
+
+    public HomePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+
+    By signInButton = By.className("login");
+    String homePageURL = "http://automationpractice.com/index.php";
+    String homePageTitle = "My Store";
+
+    public void pressSignInButton(){
+
+        driver.findElement(signInButton).click();
+
+    }
+
+    public void openHomePage(){
+        driver.get(homePageURL);
+    }
+
+    public void checkIfYouAreOnTheHomePage(){
+        String currentTitle = driver.getTitle();
+        Assert.assertEquals(currentTitle, homePageTitle);
+    }
+
+}
